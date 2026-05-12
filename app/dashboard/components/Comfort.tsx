fea56f46fc37e4dd16b1aacc255659e6ac474929
Pedro F <pedrospf2018@gmail.com>
2026-05-12 13:14:15 -0300

    feat: update comfort level messages and add new images for enhanced user experience

--- app/dashboard/components/Comfort.tsx
+++ app/dashboard/components/Comfort.tsx
@@ -10,12 +10,19 @@ function getComfortLevel(totalMinutes: number) {
             gif: "/images/sleepUnc.gif",
             message: "Parece que o Reizinho não tem sessões registradas para hoje. Que tal começar uma nova sessão e registrar seus estudos?"
         }
-    } else if (totalMinutes < 400) {
+    } else if (totalMinutes < 300) {
         return {
             gif: "/images/studyUnc.gif",
             message: "O Reizinho teve um início de dia tranquilo, mas ainda tem muito potencial para continuar!"
         }
-    } else {
+    } else if (totalMinutes < 420) {
+        return {
+            gif: "/images/uncledoctor.jpg",
+            message: "O Reizinho está indo muito bem hoje! Continue assim, mas lembre-se de fazer pausas para descansar."
+        }
+    }
+    // Maior que 420minutos
+    else {
         return {
             gif: "/images/tiredUnc.gif",
             message: "Uau, o Reizinho já acumulou um tempo de estudo impressionante hoje! Parece que a motivação está em alta. Descanse um pouco."
@@ -31,8 +38,11 @@ export function ComfortSection() {
 
 
     if (user?.name !== "Laura") return null; // Se não for a Laura, não mostramos a seção de conforto
+
     const totalMinutes = logs?.reduce((sum, log) => sum + log.duration_minutes, 0) || 0;
     const level = getComfortLevel(totalMinutes);
+
+
     return (
         <div className="bg-linear-to-r h-40 from-primary/10 to-secondary/10 rounded-lg p-4 flex items-center gap-6">
             <div className="relative shrink-0 ">
